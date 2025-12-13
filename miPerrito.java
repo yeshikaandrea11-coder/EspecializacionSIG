ea19c33827f14dedaa41e5b7217574fe4bf31ef5
yeshikaandrea11-coder <yeshikaandrea11@gmail.com>
2025-12-13 07:53:40 -0500

    Update miPerrito.java

--- miPerrito.java
+++ miPerrito.java
@@ -8,27 +8,27 @@ public class miPerrito{
 	miPerrito(String nombre, String raza,int años, String color){
 	this.nombre=nombre;
 	this.raza=raza;
-	this.tipo=tipo;
 	this.color=color;
-	
+	}
 	public void CumplirAnios(int valor){
 		años=años+valor;
-		System.out.println(""+nombre+" tiene "+años+" años");
+		System.out.println("+nombre+ tiene "+años+" años");
 		
  	public void irVeterinario(){
 		for(int i=revisionEnfermedad; i<3;i++){
-			System.out.println("+nombre+" cada mes 3 meses debe ir al veterinario, voy en el mes "+i"");
+			System.out.println("+nombre+ cada mes 3 meses debe ir al veterinario, voy en el mes "+i"");
 			revisionEnfermedad=i;
 		}
 	}
 	
 	public void baño(boolean sucio){
-		if(sucio=true){
-			System.out.println("+nombre+" necesita un baño");
+		if(sucio==true){
+			System.out.println("+nombre+ necesita un baño");
 		}else{
-			System.out.println("+nombre+" no necesita bañarse, esta limpio");
+			System.out.println("+nombre+ no necesita bañarse, esta limpio");
 		}
 	}
 			
 }
+
 
